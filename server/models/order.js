15f3b60677ad319abe0c6d00823ddf20cbd6c89e
FuYaoDe <dan82625@gmail.com>
2016-06-24 15:51:47 +0800

    add allpay model spec

--- server/models/order.js
+++ server/models/order.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.FLOAT,
       field: 'should_trade_amt',
     },
-    account: DataTypes.STRING,
+    paidAccount: {
+      type: DataTypes.STRING,
+      field: 'paid_account',
+    },
   }, {
     classMethods: {
       associate: (models) => {
---
+++ test/server/models/order.spec.js
@@ -0,0 +1,26 @@
+describe.only('about report model operation.', () => {
+  it('create report should success.', async (done) => {
+    try {
+      const allpay = await models.Allpay.create({
+        trade_no: '201203151740582564',
+        merchant_trade_no: '2000132816da8db1',
+        rtn_code: 1,
+        rtn_msg: 'paid',
+        payment_date: '2012-03-16 04:03:12',
+        payment_type: 'ATM_TAISHIN',
+        trade_amt: 22000,
+      });
+      const order = await models.Order.create({
+        paymentDate: '2012-03-16 04:03:12',
+        price: 22000,
+        paidAccount: '9103522175887271',
+        allpay_id: allpay.id,
+      });
+      console.log(JSON.stringify(order, null, 2));
+      done();
+    } catch (e) {
+      console.log(e);
+      done(e)
+    }
+  });
+});
