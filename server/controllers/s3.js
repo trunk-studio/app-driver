05a5fe370201ebf8925d392bdd71d5492bb4d52f
hinx <b2765270@gmail.com>
2016-06-30 00:29:08 +0800

    add s3 upload controller spec

--- server/controllers/s3.js
+++ server/controllers/s3.js
@@ -1,6 +1,13 @@
-exports.upload = async (ctx, params) => {
+exports.upload = async (ctx) => {
   try {
+    const data = ctx.request.body;
+    const params = {
+      Bucket: data.Bucket,
+      Key: data.Key,
+      Body: JSON.stringify(data.Body),
+    };
     const result = await services.s3.upload(params);
+
     ctx.body = result ? { status: 'ok' } : { status: 'fail' };
   } catch (e) {
     ctx.body = { error: e };
