f4c82d8c8ac521bf9e2ea989de6b56ab9b76c1fe
hinx <b2765270@gmail.com>
2016-06-30 00:29:36 +0800

    update description of s3 upload service spec

--- test/server/services/s3.spec.js
+++ test/server/services/s3.spec.js
@@ -1,16 +1,15 @@
 import AWS from 'aws-sdk';
 
-describe.only('about S3 service', () => {
+describe('about S3 service', () => {
   const params = {
     Bucket: 'app-driver',
     Key: 'testfile',
     Body: JSON.stringify({ test: 'testing' }),
   };
-
   const s3 = new AWS.S3();
   const putObjectPromise = s3.putObject(params).promise();
 
-  it('file should be uploaded to s3 successfully', async (done) => {
+  it('JSON object should be uploaded successfully by invoking service', async (done) => {
     try {
       const result = await putObjectPromise.then(() => {
         console.log('uploaded!');
